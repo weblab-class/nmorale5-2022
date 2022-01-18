fa9e7cc7c55129f753933fbf70d41f7d3583c501
Noah Morales <super@LAPTOP-I972FERD.mit.edu>
2022-01-18 17:49:24 -0500

    added .env stuff

--- server/server.js
+++ server/server.js
@@ -13,6 +13,8 @@
 | - Actually starts the webserver
 */
 
+require('dotenv').config();
+
 // validator runs some basic checks to make sure you've set everything up correctly
 // this is a tool provided by staff, so you don't need to worry about it
 const validator = require("./validator");
@@ -33,7 +35,7 @@ const auth = require("./auth");
 
 // Server configuration below
 // TODO change connection URL after setting up your team database
-const mongoConnectionURL = "mongodb+srv://noah:dontforgetme973@cluster0.dlivr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const mongoConnectionURL = process.env.ATLAS_SRV;
 // TODO change database name to the name you chose
 const databaseName = "Game";
 
@@ -57,7 +59,7 @@ app.use(express.json());
 // set up a session, which will persist login data across requests
 app.use(
   session({
-    secret: "session-secret",
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
   })
@@ -95,7 +97,7 @@ app.use((err, req, res, next) => {
 });
 
 // hardcode port to 3000 for now
-const port = 3000;
+const port = process.env.PORT || 3000;
 const server = http.Server(app);
 // socketManager.init(server);
 
